e94d5d9a6dbce74e2d1bbc08c8ff4424c4a0fdf2
Jared Brown <jaredbrown1@hotmail.com>
2021-07-22 19:42:09 -0700

    home component completed

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Footer from "../src/components/footer/Footer";
 import FooterHeader from "../src/components/footer-header/FooterHeader";
 import HomeTop from "../src/components/home/home-top/HomeTop";
 import HomeMiddle from "../src/components/home/home-middle/HomeMiddle";
+import HomeBottom from "../src/components/home/home-bottom/HomeBottom";
 
 export default function Home() {
 	return (
@@ -10,6 +11,7 @@ export default function Home() {
 			<NavBar />
 			<HomeTop />
 			<HomeMiddle />
+			<HomeBottom />
 			<FooterHeader />
 			<Footer />
 		</div>
---
+++ styles/component-styles/home-styles/HomeBottom.module.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const HomeBottomContainer = styled.div`
+	margin: 0 auto;
+	max-width: 1111px;
+	height: 412px;
+	margin-bottom: 120px;
+	display: flex;
+	color: var(--col-primary-two);
+	justify-content: space-around;
+	text-align: center;
+	h1 {
+		font-size: 20px;
+		font-weight: 500;
+		letter-spacing: 5px;
+	}
+	p {
+		font-size: 16px;
+	}
+`;
