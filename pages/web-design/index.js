aef78066d3f89f2ac452ad34069e2edb915ed923
Jared Brown <jaredbrown1@hotmail.com>
2021-08-08 15:46:28 -0700

    web design page completed

---
+++ pages/web-design/index.js
@@ -0,0 +1,98 @@
+import NavBar from "../../src/components/nav-bar/NavBarLight";
+import Footer from "../../src/components/footer/Footer";
+import FooterHeader from "../../src/components/footer-header/FooterHeader";
+import DesignTitle from "../../src/components/design/DesignTitle";
+import DesignCard from "../../src/components/design/DesignCard";
+import Image1 from "../../public/design-images/image-express.jpg";
+import Image2 from "../../public/design-images/image-transfer.jpg";
+import Image3 from "../../public/design-images/image-photon.jpg";
+import Image4 from "../../public/design-images/image-builder.jpg";
+import Image5 from "../../public/design-images/image-blogr.jpg";
+import Image6 from "../../public/design-images/image-camp.jpg";
+import DesignNextApp from "../../src/components/design/DesignNextApp";
+import DesignNextGraphic from "../../src/components/design/DesignNextGraphic";
+
+const WebDesign = () => {
+	return (
+		<div>
+			<div style={{ margin: "0 auto", width: "1120px" }}>
+				<NavBar />
+				<DesignTitle
+					title="Web Design"
+					subtitleOne="We build websites that serve as powerful marketing tools"
+					subtitleTwo="and bring memorable brand experiences."
+				/>
+
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						padding: "10px",
+					}}
+				>
+					<DesignCard
+						image={Image1}
+						title="EXPRESS"
+						subtitleOne="A multi-carrier shipping website for"
+						subtitleTwo="ecommerce businesses"
+					/>
+					<DesignCard
+						image={Image2}
+						title="TRANSFER"
+						subtitleOne="Site for low-cost money transfers and"
+						subtitleTwo="sending money withing seconds"
+					/>
+					<DesignCard
+						image={Image3}
+						title="PHOTON"
+						subtitleOne="A state-of-the-art music player with"
+						subtitleTwo="high-resolution audio and DSP effects"
+					/>
+				</div>
+
+				<div
+					style={{
+						display: "flex",
+						marginBottom: "80px",
+					}}
+				>
+					<DesignCard
+						image={Image4}
+						title="BUILDER"
+						subtitleOne="Connects users with local contractors"
+						subtitleTwo="based on their location"
+					/>
+					<DesignCard
+						image={Image5}
+						title="BLOGR"
+						subtitleOne="Blogr is a platform for creating an online"
+						subtitleTwo="blog or publication"
+					/>
+					<DesignCard
+						image={Image6}
+						title="CAMP"
+						subtitleOne="Get expert training in coding, data,"
+						subtitleTwo="design, and digital marketing"
+					/>
+				</div>
+
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						paddingRight: "25px",
+						paddingBottom: "80px",
+					}}
+				>
+					<DesignNextApp />
+					<DesignNextGraphic />
+				</div>
+			</div>
+
+			<FooterHeader />
+			<Footer />
+		</div>
+	);
+};
+
+export default WebDesign;
